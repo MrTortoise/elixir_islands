b1eee725a5ad97485e4af4f6baeff15fe2dd3bdc
John Nicholas <john.nicholas@dinoroar.io>
2022-03-05 15:01:41 +0000

    can add islands to a board

--- islands_engine/lib/islands_engine/game.ex
+++ islands_engine/lib/islands_engine/game.ex
@@ -1,7 +1,8 @@
 defmodule IslandsEngine.Game do
   use GenServer
 
-  alias IslandsEngine.{Board, Guesses, Rules}
+  alias IslandsEngine.{Board, Guesses, Rules, Island, Coordinate}
+  @players [:player1, :player2]
 
   def start_link(name) when is_binary(name), do: GenServer.start_link(__MODULE__, name, [])
 
@@ -13,11 +14,26 @@ defmodule IslandsEngine.Game do
   iex> {:ok, game} = Game.start_link("Dave")
   iex> Game.add_player(game, "dave2")
   :ok
-  iex> state = :sys.get_state(game)
-  iex> state.player2.name == "dave2"
+  iex> :sys.get_state(game).player2.name == "dave2"
   true
   """
   def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
+
+  @doc """
+  allows players to position IslandsEngine.Island
+
+  ## Example
+
+  ### Happy PAth
+  iex> alias IslandsEngine.Game
+  iex> {:ok, game} = Game.start_link("Dave")
+  iex> Game.add_player(game, "dave2")
+  iex> Game.position_island(game, :player1, :square, 1, 1)
+  :ok
+
+  """
+  def position_island(game, player, shape, row, col) when player in @players,
+    do: GenServer.call(game, {:position_island, player, shape, row, col})
 
   def init(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
@@ -39,4 +55,28 @@ defmodule IslandsEngine.Game do
   defp update_player2_name(state, name), do: put_in(state.player2.name, name)
   defp update_rules(state, rules), do: %{state | rules: rules}
   defp reply_success(state, reply), do: {:reply, reply, state}
+
+  def handle_call({:position_island, player, shape, row, col}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:position_islands, player}),
+         {:ok, coordinate} <- Coordinate.new(row, col),
+         {:ok, island} <- Island.new(shape, coordinate),
+         %{} = board <- Board.position_island(board, shape, island) do
+      state
+      |> update_board(player, board)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:reply, :error, state}
+      {:error, :invalid_coordinates} -> {:reply, {:error, :invalid_coordinates}, state}
+      {:error, :invalid_island_type} -> {:reply, {:error, :invalid_island_type}, state}
+      {:error, :overlapping_island} -> {:reply, {:error, :overlapping_island}, state}
+    end
+  end
+
+  defp player_board(state, player), do: Map.get(state, player).board
+
+  defp update_board(state, player, board),
+    do: Map.update!(state, player, fn p -> %{p | board: board} end)
 end
--- islands_engine/test/game_test.exs
+++ islands_engine/test/game_test.exs
@@ -1,4 +1,30 @@
 defmodule GameTest do
   use ExUnit.Case
   doctest IslandsEngine.Game
+
+  alias IslandsEngine.{Game}
+
+  test "can add players to game" do
+    {:ok, game} = Game.start_link("dave")
+    :ok = Game.add_player(game, "fred")
+    assert :sys.get_state(game).player2.name == "fred"
+  end
+
+  defp game_with_players_set(context) do
+    {:ok, game} = Game.start_link("dave")
+    :ok = Game.add_player(game, "fred")
+    %{game: game}
+  end
+
+  describe "Given a game with 2 players added" do
+    setup [:game_with_players_set]
+
+    test "add an island", context do
+      game = context.game
+      :ok = Game.position_island(game, :player1, :square, 1, 1)
+      state = :sys.get_state(game)
+      board = state.player1.board
+      assert Map.has_key?(board, :square)
+    end
+  end
 end
